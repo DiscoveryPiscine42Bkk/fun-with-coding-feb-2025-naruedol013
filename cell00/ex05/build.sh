60cdd75a3a4eb773674ac0b369f0cd133b027f6e
naruedol013 <naruedol.sarawut@gmail.com>
2025-02-07 01:04:00 +0700

    Update build.sh

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
-# Check if no arguments were supplied
-if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
-else
-    # Loop through each argument and create a folder with "ex" prefix
-    for arg in "$@"; do
-        mkdir -p "ex$arg"
-    done
+if [ "$#" -eq 0 ]
+then
+  echo "No arguments supplied"
+  exit 1
 fi
+for i in "$@"
+do
+    mkdir ex$i
+done
