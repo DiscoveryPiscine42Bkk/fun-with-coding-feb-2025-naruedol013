af8bb8c58e4bdc006b11588f127b2a046f8dd235
naruedol013 <naruedol.sarawut@gmail.com>
2025-02-06 22:57:24 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-# Check if no arguments were supplied
 if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
-else
-    # Print only the first three arguments
-    for arg in "$1" "$2" "$3"; do
-        # If the argument is empty (fewer than 3 args were provided), break
-        [ -z "$arg" ] && break
+    echo "no arguments found"
+
+elif [ $# -gt 1 ] && [ $# -le 3 ]; then
+    for arg in "$@"; do
         echo "$arg"
     done
 fi
