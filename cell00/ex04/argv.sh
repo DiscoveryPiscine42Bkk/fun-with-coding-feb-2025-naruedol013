b125e373982f37ef1710213ed8170fa5e6205784
naruedol013 <naruedol.sarawut@gmail.com>
2025-02-07 01:07:54 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,7 +1,7 @@
 if [ $# -eq 0 ]; then
     echo "no arguments found"
 
-elif [ $# -gt 1 ] && [ $# -le 3 ]; then
+elif [ $# -gt 0 ] && [ $# -le 3 ]; then
     for arg in "$@"; do
         echo "$arg"
     done
